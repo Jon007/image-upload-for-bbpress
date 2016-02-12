a773289f69d7a6878a66dd7f44188de50e8758b1
hearken <hearken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-02-12 06:14:48 +0000

    Bugfix: non-root-URL WordPress installs
    git-svn-id: http://plugins.svn.wordpress.org/image-upload-for-bbpress/trunk@1348724 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/bbp-image-upload.js
+++ js/bbp-image-upload.js
@@ -1,12 +1,12 @@
 /**
- * Author: Hearken Media
+ * Author: Potent Plugins
  * License: GNU General Public License version 2 or later
  * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
  */
 function hm_bbpui_file_upload(field_id) {
 	jQuery('#hm_bbpui_file_form, #hm_bbpui_file_frame').remove();
 	jQuery('#' + field_id).parent().css('overflow', 'hidden')
-		.append('<br /><form id="hm_bbpui_file_form" action="/?hm_bbpui_do_upload=1" method="post" target="hm_bbpui_file_frame" enctype="multipart/form-data" onsubmit="hm_bbpui_file_upload_submit();" data-field-id="' + field_id + '">' +
+		.append('<br /><form id="hm_bbpui_file_form" action="' + hm_bbpui_upload_url + '" method="post" target="hm_bbpui_file_frame" enctype="multipart/form-data" onsubmit="hm_bbpui_file_upload_submit();" data-field-id="' + field_id + '">' +
 		'<input type="file" name="hm_bbpui_file" id="hm_bbpui_file" onchange="jQuery(this).parent().submit();" />' +
 		'<iframe id="hm_bbpui_file_frame" name="hm_bbpui_file_frame" style="width: 1px; height: 1px; border: 0;" />');
 	jQuery('#hm_bbpui_file').trigger('click');
